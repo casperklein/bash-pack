e93af853c5bf83f9bdedddf02e9fe69a9149f7c0
casperklein <casperklein@users.noreply.github.com>
2017-09-03 00:48:46 +0200

    new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get update
+apt-get install aptitude boxes bsdmainutils ccze checkinstall colordiff colormake curl dcfldd hexdump lynx most openssl procps pv tcpflow vim
+
+cd /scripts &&
+	wget https://raw.githubusercontent.com/casperklein/croncal/master/croncal.pl
