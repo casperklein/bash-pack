f52f448fe63450d270ff329fee58d5f9b80d8d1d
casperklein <casperklein@users.noreply.github.com>
2017-09-03 00:54:49 +0200

    modified:   install.sh new file:   tmux.conf

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@
 apt-get update
 apt-get install aptitude boxes bsdmainutils ccze checkinstall colordiff colormake curl dcfldd lynx most openssl procps pv tcpflow vim
 
-cd /scripts &&
-	wget https://raw.githubusercontent.com/casperklein/croncal/master/croncal.pl
+
