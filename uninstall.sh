f4ca59529c410641e1e71bac5fac3f6621660ad9
casperklein <casperklein@users.noreply.github.com>
2017-09-20 23:24:43 +0200

    modified:   uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,11 +3,15 @@ if [ "$(readlink -f ~/.bashrc)" == "/scripts/bashrc" ] && [ -f ~/.bashrc.backup 
 	echo 'Restoring' ~/.bashrc.backup '->' ~/.bashrc
 	rm ~/.bashrc &&
 	mv ~/.bashrc.backup ~/.bashrc
+else
+	echo 'Error:' ~/.bashrc 'symlink or' ~/.bashrc.backup 'not found.' >&2
 fi
 
 if [ "$(readlink -f /etc/vim/vimrc.local)" == "/scripts/vimrc.local" ]; then
 	echo 'Removing symlink /etc/vim/vimrc.local -> /scripts/vimrc.local'
 	rm /etc/vim/vimrc.local
+else
+	echo 'Error: /etc/vim/vimrc.local not found' >&2
 fi
 
 [ -f ~/.bashrc.local ] && rm -i ~/.bashrc.local
