94f6a6e8a0d79dacd008afe870ab07a01f0e0431
casperklein <casperklein@users.noreply.github.com>
2017-09-28 01:40:45 +0200

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-cd /scripts && git pull
+
+# running as root?
+if [ "$(id -u)" == "0" ]; then
+	cd /scripts &&
+	git pull
+else
+	echo "Error: You must be root to run $0" >&2
+	echo
+fi
