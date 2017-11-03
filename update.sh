627dcc9a506b24f4d25f175f6bcc1e9614919eeb
casperklein <casperklein@users.noreply.github.com>
2017-11-03 23:47:51 +0100

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,16 @@
 # running as root?
 if [ "$(id -u)" == "0" ]; then
 	cd /scripts &&
+	git fetch
+	DIFF=$(PAGER=cat git log HEAD..origin)
+	if [ ! -z "$DIFF" ]; then
+	       	echo "Commits to pull:"
+		echo
+		echo "$DIFF"
+		echo
+		read -p "Press ENTER to continue.."
+		echo
+	fi
 	git pull
 else
 	echo "Error: You must be root to run $0" >&2
