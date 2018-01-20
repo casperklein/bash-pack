1298645a5c27b3bba8951d729e426fb46855c7b3
casperklein <casperklein@users.noreply.github.com>
2018-01-20 03:25:25 +0100

    new file:   repair.sh

---
+++ repair.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPTS=$(dirname "$(readlink -f "$0")")
+
+# running as root?
+if [ "$(id -u)" == "0" ]; then
+	cd $SCRIPTS &&
+	git fetch --all
+	git reset --hard origin/master
+	git status
+else
+	echo "Error: You must be root to run $0" >&2
+	echo
+fi
