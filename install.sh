904ae6fd2dfb6ed1a00bbe91955cafb67f07a622
casperklein <casperklein@users.noreply.github.com>
2018-03-01 14:14:58 +0100

    install.sh: net-tools package added.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ SCRIPTS=$(dirname "$(readlink -f "$0")")
 
 if [ $ROOT ]; then
 	apt-get update &&
-	apt-get install aptitude bash-completion bc boxes bsdmainutils ccze checkinstall colordiff colormake coreutils curl findutils git htop lynx mlocate most openssl procps pv tar tcpflow unzip vim wget
+	apt-get install aptitude bash-completion bc boxes bsdmainutils ccze checkinstall colordiff colormake coreutils curl findutils git htop lynx mlocate most net-tools openssl procps pv tar tcpflow unzip vim wget
 	# called from repair.sh?
 	[ $# -gt 0 ] && exit 
 fi
