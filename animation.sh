af7938bb88c5e5fdbb1d23ee032647a878a65062
casperklein <casperklein@users.noreply.github.com>
2022-07-03 02:29:02 +0200

    animation improved

--- animation.sh
+++ animation.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
 
-startAnimation() {
-	if [ -n "$APID" ]; then
+_animationStart() {
+	if [ -n "${APID:-}" ]; then
 		echo
 		echo "Error: Animation already running. Call stopAnimation() first." >&2
 		echo
 		exit 1
 	fi
 	tty -s || return 0 # no tty? no animation!
-	CODE='
-		DELETE="\033[1D"
-		SLEEP=0.2
+
+	tput civis # hide cursor
+
+	# shellcheck disable=SC2016
+	local CODE='
+		ANIMATION=("\\" "|" "/" "-")
+		DELETE=$(echo -ne "\033[1D")
 		while :; do
-		echo -en "\\"
-		sleep $SLEEP
-		echo -en "$DELETE|"
-		sleep $SLEEP
-		echo -en "$DELETE/"
-		sleep $SLEEP
-		echo -en "$DELETE-"
-		sleep $SLEEP
-		echo -en "$DELETE"
+			for i in "${ANIMATION[@]}"; do
+				echo -n "$i"
+				sleep 0.2
+				echo -n "$DELETE"
+			done
 		done
 	'
 
 	echo "$CODE" | bash &
 	APID=$!
-	APIDFILE=$(mktemp /tmp/"${0##*/}".animation.XXX.pid)
-	echo $APID > "$APIDFILE"
 }
 
-stopAnimation() {
-	if [ -n "$APID" ]; then
-		kill -9 $APID &>/dev/null
-		wait $APID &>/dev/null	# hide kill message from background job: https://stackoverflow.com/a/5722850/568737
+_animationStop() {
+	tput cnorm # show cursor
+
+	if [ -n "${APID:-}" ]; then
+		kill -9 "$APID" &>/dev/null
+		wait "$APID" &>/dev/null || true	# hide kill message from background job: https://stackoverflow.com/a/5722850/568737
 		echo -ne "\033[1D\033[K"
 		APID=
-		[ -n "$APIDFILE" ] && rm "$APIDFILE"
+		# [ -n "$APIDFILE" ] && rm "$APIDFILE"
 	fi
 }
 
-<<"comment"
-# if argument is given, start a 5 second demo
-if [ ! -z "$1" ]; then
-	trap stopAnimation EXIT
-
-	echo -n "Working hard.. "
-	startAnimation
-	#startAnimation # fore error
-	sleep 5
-	stopAnimation && echo
+# EXIT trap already set?
+if [ -z "$(trap -p EXIT)" ]; then
+	trap _animationStop EXIT
+else
+	# append _animationStop() to existing trap
+	# shellcheck disable=SC2064
+	trap "$(trap -p EXIT | cut -d"'" -f2); _animationStop" EXIT
 fi
-comment
+
+# not sourced? show demo
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+	echo -n "Working hard.. "
+	_animationStart
+	sleep 3
+	_animationStop
+	echo
+fi
