149a17db795a10fe280a6494c1770e872c91a6b5
casperklein <casperklein@users.noreply.github.com>
2023-01-27 02:21:27 +0100

    full wipe added

--- repair.sh
+++ repair.sh
@@ -9,12 +9,33 @@ if [ "$(id -u)" == "0" ]; then
 	cd "$SCRIPTS"
 	echo "Wiping local changes.."
 	echo
-	git fetch
-	git reset --hard origin/master
+
+	if [ "${1:-}" == "--hard" ]; then
+		# save origin if possible
+		ORIGIN=$(git remote get-url origin 2>/dev/null || echo 'https://github.com/casperklein/bash-pack.git')
+		# complete wipe
+		rm -rf .git
+		# start from scratch
+		git init --quiet
+		git remote add origin "$ORIGIN"
+		git fetch origin
+		git remote set-head -a origin
+		git reset --hard origin/master
+		git branch --set-upstream-to=origin/master
+	else
+		# shellcheck disable=2015
+		git fetch && git reset --hard origin/master || {
+			echo
+			echo "Something went wrong. You may try: $0 --hard"
+			exit 1
+		} >&2
+	fi
+
+	echo
+	"$SCRIPTS"/install.sh
 	echo
 	git status
 	echo
-	"$SCRIPTS"/install.sh
 else
 	echo "Error: You must be root to run '$0'" >&2
 	echo >&2
